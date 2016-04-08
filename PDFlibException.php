08c1806de3d1abf790505c4eba2e810d4ec63853
Jackson Tong <jackson@snapfrozen.com.au>
2016-04-08 21:13:35 +1000

    fix wrong namespace

--- PDFlibException.php
+++ PDFlibException.php
@@ -1,42 +1,19 @@
 <?php
 
-namespace toantv;
+namespace tongtoanbs;
 
 /**
  * Class PDFlibException
  *
  * @author Jackson Tong <jackson@snapfrozen.com.au>
+ *
+ * @method integer get_errnum() The error code of the most recent error condition
+ * @method string get_apiname() Get the name of the API function
+ * which threw the last exception or failed.
+ * @method string get_errmsg() Get the text of the last thrown exception
+ * or the reason of a failed function call.
  */
 class PDFlibException extends \PDFlibException
 {
-    /**
-     * @return integer The error code of the most recent error condition
-     */
-    public function get_errnum()
-    {
-        return parent::get_errnum();
-    }
 
-    /**
-     * Get the name of the API function
-     * which threw the last exception or failed.
-     * @return string The name of the API function which threw an exception,
-     * or the name of the most recently called function
-     * which failed with an error code.
-     */
-    public function get_apiname()
-    {
-        return parent::get_apiname();
-    }
-
-    /**
-     * Get the text of the last thrown exception
-     * or the reason of a failed function call.
-     * @return string Text containing the description
-     * of the most recent error condition.
-     */
-    public function get_errmsg()
-    {
-        return parent::get_errmsg();
-    }
 }
